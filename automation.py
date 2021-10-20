ef02c6260e665c6b71131b342aef117907a2431b
Sergio <sergio.leiva@sngular.com>
2021-10-19 22:21:14 -0400

    Finish test

--- automation.py
+++ automation.py
@@ -9,7 +9,19 @@ assert 'Selenium Easy' in chrome_browser.title # Fail in case is false
 
 # <button type="button" onclick="showInput();" class="btn btn-default">Show Message</button>
 
-button = chrome_browser.find_element_by_class_name('btn-default') # Find specific class
+show_message_button = chrome_browser.find_element_by_class_name('btn-default') # Find specific class
+print(show_message_button.get_attribute('innerHTML'))
 
-print(button.get_attribute('innerHTML'))
-chrome_browser.close()
+assert 'Show Message' in chrome_browser.page_source
+
+# <input type="text" class="form-control" placeholder="Please enter your Message" id="user-message">
+user_message = chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys('I am suuuuuper cool')
+
+show_message_button.click()
+
+output_message = chrome_browser.find_element_by_id('display')
+assert 'I am suuuuuper cool' in output_message.text
+
+chrome_browser.quit()
